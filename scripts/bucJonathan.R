dbd8f7e41d56059a0bae6191375fdcf5eb52a61f
mrjjj <jonathan.romiguier@gmail.com>
2016-08-05 10:30:57 +0200

    small fixes

--- scripts/bucJonathan.R
+++ scripts/bucJonathan.R
@@ -3,6 +3,7 @@ args = commandArgs(TRUE)
 
 buc = read.table("../final_output_2.txt", sep="\t", header=T)
 bio = read.csv("../bioWithPerry.csv")
+bio = read.csv("../bio.csv")
 biobuc = merge(bio,buc, by.x="focal", by.y="dataset")
 
 attach(biobuc)
@@ -16,7 +17,9 @@ text(log(juvSize), r_expression_ENcP, sub=cor.test(log(juvSize), r_expression_EN
 plot(piS, r_expression_ENcP, sub=cor.test(piS, r_expression_ENcP)$p.val, main=(cor.test(piS, r_expression_ENcP)$estimate)^2)
 text(log(piS), r_expression_ENcP, sub=cor.test(log(piS), r_expression_ENcP)$p.val, main=cor.test(log(piS), r_expression_ENcP)$estimate, focal)
 
-plot(log(longevity), r_expression_ENcP, sub=cor.test(log(longevity), r_expression_ENcP)$p.val, main=(cor.test(log(longevity), r_expression_ENcP, method="kendall")$estimate)^2, bg='grey60', pch=21, cex=1.5)
+plot(log(longevity), r_expression_ENcP, sub=cor.test(log(longevity), r_expression_ENcP)$p.val, main=(cor.test(log(longevity), r_expression_ENcP, method="pearson")$estimate)^2, bg='grey60', pch=21, cex=1.5)
 abline(lm(r_expression_ENcP~log(longevity)), col='red')
 text(log(longevity), r_expression_ENcP, sub=cor.test(log(longevity), r_expression_ENcP)$p.val, main=(cor.test(log(longevity), r_expression_ENcP)$estimate)^2, focal)
-dev.print(pdf,'longevityBuc.pdf')
+dev.print(pdf,'../figures/longevityBuc.pdf')
+
+plot(log(sexmat), r_expression_ENcP, sub=cor.test(log(sexmat), r_expression_ENcP)$p.val, main=(cor.test(log(sexmat), r_expression_ENcP, method="pearson")$estimate)^2, bg='grey60', pch=21, cex=1.5)
